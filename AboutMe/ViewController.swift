86e0526008f9da776e98e6b2893137d03a6af388
victor <ruizdvictor@gmail.com>
2019-04-23 12:40:32 -0400

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Victor  on 4/23/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("My name is Victor")
+    }
+
+
+}
+
